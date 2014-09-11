4708453fda42db3d93fe16f6bdcfe9fd4af3685f
Andrew Mukiza <andrewmukiza@gmail.com>
2014-09-11 16:00:04 +0300

    running on new port

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-var http = require("http");
+var http = require("http"),
+		port = 8084;
 
 function start() {
   function onRequest(request, response) {
-    console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
   }
 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+  http.createServer(onRequest).listen(port);
+  console.log("Server has started " + port);
 }
 
 exports.start = start;
