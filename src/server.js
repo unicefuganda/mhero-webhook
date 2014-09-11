8c82e1c8f38582205cb0b46c3cfdd56813d573bb
Andrew Mukiza <andrewmukiza@gmail.com>
2014-09-11 16:59:59 +0300

    adding dummy response

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 var http = require("http"),
-		port = 8084;
+    port = 8084;
 
 function start() {
-  function onRequest(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
+    function onRequest(request, response) {
+        var responseBody = JSON.stringify({
+            "urns": ["tel:+250788123123", "tel:+250788123124"],
+            "text": "hello world"
+        });
+        response.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
 
-  http.createServer(onRequest).listen(port);
-  console.log("Server has started " + port);
+        response.write(responseBody);
+
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(port);
+    console.log("Server has started " + port);
 }
 
 exports.start = start;
