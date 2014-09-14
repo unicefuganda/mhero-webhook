bd4666620ace17206c71514e48b2b9fd362aa6ea
Andrew Mukiza <andrewmukiza@gmail.com>
2014-09-14 10:35:23 +0300

    bounce back the request params

--- src/server.js
+++ src/server.js
@@ -1,23 +1,15 @@
-var http = require("http"),
-    port = 8084;
+var url = require('url'),
+	http = require('http')
+	port = 8084;
 
 function start() {
-    function onRequest(request, response) {
-        var responseBody = JSON.stringify({
-            "urns": ["tel:+250788123123", "tel:+250788123124"],
-            "text": "hello world"
-        });
-        response.writeHead(200, {
-            "Content-Type": "text/plain"
-        });
-
-        response.write(responseBody);
-
-        response.end();
-    }
-
-    http.createServer(onRequest).listen(port);
-    console.log("Server has started " + port);
+	http.createServer(function(request, response) {
+		var query = url.parse(request.url, true).query;
+		response.writeHead(200);
+		response.write(JSON.stringify(query));
+		response.end();
+	}).listen(port);
+	console.log("Server has started " + port);
 }
 
 exports.start = start;
