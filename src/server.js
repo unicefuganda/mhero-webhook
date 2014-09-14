eb5ee30da40b070bf13dc7626471e77a08bbb63e
Andrew Mukiza <andrewmukiza@gmail.com>
2014-09-14 11:12:12 +0300

    save messages on after recieving request

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 var url = require('url'),
 	http = require('http')
-	port = 8084;
+	port = 8084,
+	Message = require(__dirname + '/../src/message');
 
 function start() {
 	http.createServer(function(request, response) {
 		var query = url.parse(request.url, true).query;
 		response.writeHead(200);
-		response.write(JSON.stringify(query));
-		response.end();
+		var message = new Message(query);
+		Message.create(message.formattedForSQlite()).then(function(){
+			response.write(JSON.stringify({message: "Your message has been send to a proffesional counsellor."}));
+			response.end();
+		})
 	}).listen(port);
 	console.log("Server has started " + port);
 }
